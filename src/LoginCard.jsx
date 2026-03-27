f46219b41f924a1122f8709be0022524cf4ac49d
Drishti Gupta <thedrishtigupta@gmail.com>
2026-03-27 23:35:55 +0530

    Learned to handle message input using useState hook

--- src/LoginCard.jsx
+++ src/LoginCard.jsx
@@ -2,13 +2,22 @@ import { useState } from "react";
 
 export const LoginCard = () => {
     const [isLoggedIn, setLogin] = useState(false);
+    const [message, setMessage] = useState("");
+
+    const handleChange = (event) => {
+        setMessage(event.target.value);
+    }
 
     const handleLogin = () => {
         setLogin(!isLoggedIn);
     }
     return (
+        <>
         <button onClick={handleLogin}>
         {isLoggedIn? "Logout" : "Login"}
         </button>
+        <input type="text" placeholder="Type a message" value={message} onChange={handleChange}/>
+        <p>{message}</p>
+        </>
     );
 };
