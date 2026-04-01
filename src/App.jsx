c962f45a7a2914a43e06ff79abdc7ed85e1932f5
Drishti Gupta <thedrishtigupta@gmail.com>
2026-04-01 16:06:14 +0530

    Add items feature in shopping cart using reducer hook

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@
 import './App.css'
 import { CounterWithReducer } from './CounterWithReducer'
 import { ShoppingCart } from './ShoppingCart'
+import { ShoppingCartWithReducer } from './ShoppingCartWithReducer'
 
 function App() {
 
   return (
     <>
+    <ShoppingCartWithReducer/>
     <CounterWithReducer/>
     <h1>Code Evolution Course</h1>
     <ShoppingCart/>
---
+++ src/ShoppingCartWithReducer.jsx
@@ -0,0 +1,89 @@
+import { useReducer } from "react";
+
+
+const initialState = {
+    items: [], // id, name, price, quantity
+    totalAmount: 0,
+    totalItems: 0,
+}
+
+const reducer = (state, action) => {
+    switch(action.type) {
+        case "ADD_ITEM": {
+            const existingItemIndex = state.items.findIndex(
+                item => item.id === action.payload.id
+            )
+            let updatedItems;
+            if(existingItemIndex >= 0) {
+                updatedItems = [...state.items];
+                updatedItems[existingItemIndex] = {
+                    ...updatedItems[existingItemIndex],
+                    quantity: updatedItems[existingItemIndex].quantity + 1,
+                };
+            } else {
+                updatedItems = [
+                    ...state.items,
+                    {
+                        ...action.payload,
+                        quantity: 1,
+                    },
+                ];
+            }
+            return {
+                ...state,
+                items:updatedItems,
+                totalAmount: updatedItems.reduce(
+                    (total, item) => total + item.price* item.quantity, 0
+                ),
+                totalItems: updatedItems.reduce((total, item) => total + item.quantity, 0)
+            }
+        }
+        default: return state;
+    }
+}
+
+export const ShoppingCartWithReducer = () => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+    const products = [
+        {id: 1, name: "React Course", price: 49.99},
+        {id: 2, name: "Node.js Course", price: 39.99},
+        {id: 3, name: "JavaScript Bundle", price: 89.99},
+    ]
+    return (
+        <div>
+        <h2>Products</h2>
+        {products.map((product) => (
+            <div key={product.id}>
+                <h3>
+                    {product.name} - ${product.price}
+                </h3>
+                <button onClick={() => dispatch({
+                    type: "ADD_ITEM",
+                    payload: product,
+                })}>Add to Cart</button>
+            </div>
+        ))}
+
+        <div>
+            <h2>Shopping Cart</h2>
+            {state.items.length === 0 ? (
+                <p>Your cart is empty</p>
+            ): (
+                <div>
+                    {
+                        state.items.map((item) => (
+                            <div key = {item.id}>
+                                <p>
+                                    {item.name} - ${item.price} x {item.quantity}
+                                </p>
+                            </div>
+                        ))
+                    }
+                </div>
+            )}
+            <h3>Total Items: {state.totalItems}</h3>
+            <h3>Total Amount: {state.totalAmount}</h3>
+        </div>
+        </div>
+    );
+};
